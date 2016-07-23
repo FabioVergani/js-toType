12d7810e22691965c00263e9cc7acd2c657fdbbe
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-07-23 15:37:19 +0200

    Create toType.js

---
+++ toType.js
@@ -0,0 +1,12 @@
+(function(){
+ function toType(x){var s=Stringfy(x);return s.slice(s.indexOf('\u0020')+1,-1).toLowerCase();};
+
+ var w=window,
+ e=w.Function.prototype,
+ Call=e.bind.bind(e.call),
+ O=w.Object,
+ Stringfy=Call((e=O.prototype).toString);
+ 
+ O.defineProperty(e,'toType',{enumerable:true,value:function(){return Stringfy(this);}});
+
+})();
