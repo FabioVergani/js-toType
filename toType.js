0426c643210c914252c3870df8a6296a663e14fd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-07-23 20:17:20 +0200

    Update toType.js

--- toType.js
+++ toType.js
@@ -1,12 +1,14 @@
 (function(){
  function toType(x){var s=Stringfy(x);return s.slice(s.indexOf('\u0020')+1,-1).toLowerCase();};
+ function isFunction(x){return Stringfy(x)==='function';};
+
 
  var w=window,
  e=w.Function.prototype,
  Call=e.bind.bind(e.call),
  O=w.Object,
  Stringfy=Call((e=O.prototype).toString);
- 
+
  O.defineProperty(e,'toType',{enumerable:true,value:function(){return Stringfy(this);}});
 
 })();
